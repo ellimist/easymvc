5cd31f7de4408ee2f1ed1a009e820c56b61a846c
Adrian Bordinc <adrian.bordinc@gmail.com>
2014-04-18 21:48:33 +0200

    Routing

--- lib/easymvc.rb
+++ lib/easymvc.rb
@@ -2,38 +2,14 @@ require "easymvc/version"
 require "easymvc/controller.rb"
 require "easymvc/utils.rb"
 require "easymvc/dependencies.rb"
+require "easymvc/routing.rb"
 
 module Easymvc
   class Application
   	def call(env)
-  		# env["PATH_INFO"] => "/pages/about" => PagesController.send(:about)
-  		# env["REQUEST_METHOD"] => GET/POST
-
-  		if env["PATH_INFO"] == "/"
-  			return [ 302, {"Location"=>"/my_pages/about"}, [] ]
-  		end
-
-  		if env["PATH_INFO"] == "/favicon.ico"
-  			return [ 500, {}, [] ]
-  		end
-
-  		controller_class, action = get_controller_and_action(env)
-      controller = controller_class.new(env)
-  		response = controller.send(action)
-
-      if controller.get_response
-        controller.get_response
-      else
-        controller.render(action)
-        controller.get_response
-      end
-
+  		return [ 500, {}, [] ] if env["PATH_INFO"] == "/favicon.ico"
+      get_rack_app(env).call(env)
   	end
 
-  	def get_controller_and_action(env)
-  		_, controller_name, action = env["PATH_INFO"].split("/")
-  		controller_name = controller_name.to_camel_case + "Controller"
-  		[ Object.const_get(controller_name), action ]
-  	end
   end
 end
